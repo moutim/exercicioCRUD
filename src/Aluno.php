199887ffbf7f314b67d3f41cd9db21ed00cf63e6
moutim <moutimg@gmail.com>
2021-12-06 13:03:18 -0300

    Adiciona getters

--- src/Aluno.php
+++ src/Aluno.php
@@ -13,5 +13,25 @@ class Aluno {
     public function __construct() {
         $this->conexao = Banco::conecta();
     }
+
     
+    // Getters
+    public function getId():int {
+        return $this->id;
+    }
+    public function getNome():int {
+        return $this->nome;
+    }
+    public function getPrimeira():int {
+        return $this->primeira;
+    }
+    public function getSegunda():int {
+        return $this->segunda;
+    }
+    public function getMedia():int {
+        return $this->media;
+    }
+    public function getSituacao():int {
+        return $this->situacao;
+    }
 }
