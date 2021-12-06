4fe766a2e15eb5d49a21dbc063f962ef88512cb4
moutim <moutimg@gmail.com>
2021-12-06 13:03:37 -0300

    Adiciona setters

--- src/Aluno.php
+++ src/Aluno.php
@@ -34,4 +34,21 @@ class Aluno {
     public function getSituacao():int {
         return $this->situacao;
     }
+
+    // Setters
+    public function setNome(string $nome) {
+        $this->nome = filter_var($nome, FILTER_SANITIZE_STRING);
+    }
+    public function setPrimeira(float $primeira) {
+        $this->primeira = filter_var($primeira, FILTER_SANITIZE_NUMBER_FLOAT);
+    }
+    public function setSegunda(string $segunda) {
+        $this->segunda = filter_var($segunda, FILTER_SANITIZE_NUMBER_FLOAT);
+    }
+    public function setMedia(string $media) {
+        $this->media = filter_var($media, FILTER_SANITIZE_NUMBER_FLOAT);
+    }
+    public function setSituacao(string $situacao) {
+        $this->situacao = filter_var($situacao, FILTER_SANITIZE_STRING);
+    }
 }
