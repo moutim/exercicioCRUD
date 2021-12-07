a8708b0936bcfa694f21fccfc6202bec84459637
moutim <moutimg@gmail.com>
2021-12-07 11:32:27 -0300

    Funcao que atualiza media em tempo real

--- atualizar.php
+++ atualizar.php
@@ -27,9 +27,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Atualizar dados - Exercício CRUD com PHP e MySQL</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" defer></script>
         <link href="css/style.css" rel="stylesheet">
-        <script src="js/script.js" defer></script>
     </head>
     <body>
         <div class="container">
@@ -69,5 +67,7 @@
     
             <hr>
     </div>
+
+    <script src="js/script.js"></script>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const segundaNota = document.getElementById('segunda');
 const media = document.getElementById('media');
 const situacao = document.getElementById('situacao');
 function calculaMedia () {
-    let notaMedia = (primeiraNota.value + segundaNota.value) / 2;
+    let notaMedia = (Number(primeiraNota.value) + Number(segundaNota.value)) / 2;
     media.setAttribute('value', notaMedia);
 
     if (notaMedia >= 7) {
@@ -12,5 +12,5 @@ function calculaMedia () {
         situacao.setAttribute('value', 'Reprovado');
     }
 }
-primeiraNota.addEventListener('change', calculaMedia());
-segundaNota.addEventListener('change', calculaMedia());
+primeiraNota.addEventListener('change', calculaMedia);
+segundaNota.addEventListener('change', calculaMedia);
